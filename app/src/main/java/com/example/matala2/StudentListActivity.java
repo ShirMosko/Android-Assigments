c2801a510bbed28977dc1ab4652066cfaef99430
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2022-12-19 22:35:16 +0200

    add list view

--- app/src/main/java/com/example/matala2/StudentListActivity.java
+++ app/src/main/java/com/example/matala2/StudentListActivity.java
@@ -3,7 +3,11 @@ package com.example.matala2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class StudentListActivity extends AppCompatActivity {
 
@@ -13,5 +17,34 @@ public class StudentListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_student_list);
 
         ListView list = findViewById(R.id.studentList_list);
+        list.setAdapter(new StudentListAdapter());
+    }
+
+    class StudentListAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int pos, View view, ViewGroup viewGroup) {
+            if(view == null){
+                view = getLayoutInflater().inflate(R.layout.student_list_row,null);
+            }
+            TextView nameTv = view.findViewById(R.id.studentlistrow_name_tv);
+            nameTv.setText("name" +pos);
+            return view;
+        }
     }
 }
